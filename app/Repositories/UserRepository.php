210895fe5d88ea7197ea73eab6f7d697008a8edb
Roberto Moreira <robertomrr@yahoo.com.br>
2024-05-08 17:23:48 -0300

    Continuação...01-Listar Usuarios da API Laravel

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -5,7 +5,15 @@ namespace App\Repositories;
 class UserRepository
 {
     public function __construct(protected User $user)
+    {        
+    }
+
+    public function getAllUsers(string $filter = ''): LengthAwarePaginator
     {
-        
+        return $this->user->where( function($query) use ($filter){
+            if ($filter !== ''){
+                $query->where('name','LIKE',"%{$filter}%" );
+            }
+        })->paginate();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('/users',[UserController::class, 'index'])->name(users.index);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/', fn () => response()->json(['message' => 'ok']));
